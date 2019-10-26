79363ed9cf86136de1573950712bf8f8945f1c75
Srbislav <srbislav@pm.me>
2019-10-26 18:02:18 +0200

    Initial commit

---
+++ events-organization/src/main/java/com/ftn/eventsorganization/EventsOrganizationApplication.java
@@ -0,0 +1,13 @@
+package com.ftn.eventsorganization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsOrganizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsOrganizationApplication.class, args);
+	}
+
+}
---
+++ events-organization/src/test/java/com/ftn/eventsorganization/EventsOrganizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.eventsorganization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsOrganizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
