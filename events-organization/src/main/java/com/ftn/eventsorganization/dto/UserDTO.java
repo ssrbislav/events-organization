38a6d824606db0cfa07121b9938bed80e31863ec
Your Name <you@example.com>
2019-11-15 03:52:53 +0100

    User CRUD

---
+++ events-organization/src/main/java/com/ftn/eventsorganization/dto/UserDTO.java
@@ -0,0 +1,89 @@
+package com.ftn.eventsorganization.dto;
+
+import java.io.Serializable;
+
+public class UserDTO implements Serializable {
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String address;
+
+    private String phoneNumber;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(String username, String password, String email, String firstName, String lastName, String address, String phoneNumber) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
