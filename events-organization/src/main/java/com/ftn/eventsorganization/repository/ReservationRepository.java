0f52a2df34347cd85b6384fbcdfdc92fc45f742a
Dana995 <37509012+Dana995@users.noreply.github.com>
2020-02-06 23:43:59 +0100

    Rezervacija

---
+++ events-organization/src/main/java/com/ftn/eventsorganization/repository/ReservationRepository.java
@@ -0,0 +1,17 @@
+package com.ftn.eventsorganization.repository;
+
+import java.sql.Date;
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ftn.eventsorganization.model.Reservation;
+
+
+@Repository
+public interface ReservationRepository extends CrudRepository<Reservation, Long> {
+	List<Reservation> findAllById(Long id);
+	List<Reservation> findAllByExpireDate(Date d);
+
+}
